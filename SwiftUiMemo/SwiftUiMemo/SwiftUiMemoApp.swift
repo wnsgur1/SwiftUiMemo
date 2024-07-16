0c6540449cb1df09c7a1aca16446d29741eea855
wnsugr1 <asdf1234@dgsw.hs.kr>
2024-07-16 16:27:54 +0900

    setting

---
+++ SwiftUiMemo/SwiftUiMemo/SwiftUiMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUiMemoApp.swift
+//  SwiftUiMemo
+//
+//  Created by dgsw8th38 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
