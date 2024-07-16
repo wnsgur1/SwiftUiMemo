9032a381b8893603af0f68f6138a7576fec69899
wnsugr1 <asdf1234@dgsw.hs.kr>
2024-07-16 17:06:45 +0900

    Step 3
    ListView 제작

--- SwiftUiMemo/SwiftUiMemo/SwiftUiMemoApp.swift
+++ SwiftUiMemo/SwiftUiMemo/SwiftUiMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUiMemoApp: App {
+    @StateObject var store = MemoStore()
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
