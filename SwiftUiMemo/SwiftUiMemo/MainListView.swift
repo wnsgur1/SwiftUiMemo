8a0409a56e37c9c7151031f13f612bbc6f7523eb
wnsugr1 <asdf1234@dgsw.hs.kr>
2024-07-16 16:34:33 +0900

    Step 1

---
+++ SwiftUiMemo/SwiftUiMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListVIew.swift
+//  SwiftUiMemo
+//
+//  Created by dgsw8th38 on 7/16/24.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
--- SwiftUiMemo/SwiftUiMemo/SwiftUiMemoApp.swift
+++ SwiftUiMemo/SwiftUiMemo/SwiftUiMemoApp.swift
@@ -13,7 +13,7 @@ struct SwiftUiMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
